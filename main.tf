728239e8af9b89134b51eb4c819a317e8ef22ffe
SaDha <SaDhaDamu@gmail.com>
2022-10-18 12:49:48 -0600

    adding vpc service

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_vpc" default" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "sample_vpc"
+  }
+}
